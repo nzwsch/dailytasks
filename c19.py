48876a453b9477da12cb6857f4e114f87ab0cfb1
Seiichi Yonezawa <nzwsch@gmail.com>
2020-07-07 17:44:25 +0900

    Fix for python 3.8

--- c19.py
+++ c19.py
@@ -1,19 +1,19 @@
-# import requests
+import requests
 from os import path
 import time
-import urlparse
+from urllib.parse import urlparse
 
 headline_link_selector = '.content > section > div > h2 > a'
-
-# page = requests.get(url)
 
 if __name__ == "__main__":
     with open('test.txt') as f:
         for line in f.readlines():
             url = line.rstrip()
-            urlpath = urlparse.urlparse(url).path
+            urlpath = urlparse(url).path
             filename = '{}.html'.format(path.basename(path.abspath(urlpath)))
             dist = path.join('html', filename)
+            page = requests.get(url)
+            print(page.status_code, url)
             time.sleep(2)
             with open(dist, 'w') as fw:
-                fw.write("hello")
+                fw.write(page.text)
