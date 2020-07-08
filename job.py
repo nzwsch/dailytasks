2bb463c77bd941dfaf93008c31a850ca75ebc727
Seiichi Yonezawa <nzwsch@gmail.com>
2020-07-08 19:09:47 +0900

    Send multiple categories

--- job.py
+++ job.py
@@ -3,6 +3,15 @@ import webhook
 import embed
 import gigazine
 import json
+import random
+import time
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
 
 if __name__ == "__main__":
     # send discord
@@ -11,34 +20,49 @@ if __name__ == "__main__":
         text = f.read()
     cats = gigazine.parse(text)
 
-    category = '新商品（衣・食・住）'
-    category_color = '#28d001'
-
-    isyokuju = cats.get(category)
-    print(len(isyokuju))
+    categories = [{'name': 'ネタ（メモ・その他いろいろ）', 'color': '#ff0094'},
+                  {'name': 'ライフスタイル（人生・生活・健康）', 'color': '#002aff'},
+                  {'name': 'IT・ガジェット（ネット・ソフト・ハード・モバイル）', 'color': '#ffa200'},
+                  {'name': 'サイエンス（科学・学問・テクノロジー）', 'color': '#aa00ff'}]
 
     embeds = []
 
-    for item in isyokuju:
-        text = item['text']
-        href = item['href']
+    for category in categories:
+        items = cats.get(category['name'])
 
-        r = requests.get(href)
-        print(r.status_code, href)
+        print(len(items), category['name'])
 
-        result = embed.parse(r.content)
+        for item in items:
+            text = item['text']
+            href = item['href']
+            category_name = 'カテゴリ◆{}'.format(category['name'])
+            color = category['color']
 
-        embeded = webhook.to_embed(title=text,
-                                   url=href,
-                                   category='カテゴリ◆{}'.format(category),
-                                   color=category_color,
-                                   author=result.get('author'),
-                                   image=result.get('image'),
-                                   description=result.get('description'))
+            r = requests.get(href)
+            print(r.status_code, href)
 
-        embeds.append(embeded)
+            result = embed.parse(r.content)
 
-    with open("result-isyokuju.json", "w") as f:
+            embeded = webhook.to_embed(title=text,
+                                       url=href,
+                                       category=category_name,
+                                       color=color,
+                                       author=result.get('author'),
+                                       image=result.get('image'),
+                                       description=result.get('description'))
+
+            embeds.append(embeded)
+
+    random.shuffle(embeds)
+
+    with open("result.json", "w") as f:
         f.write(json.dumps(embeds, ensure_ascii=False, indent=4))
 
-    webhook.post_link('2020年7月6日のヘッドラインニュース', embeds)
+    iterate = 1
+    embeded_partition = list(chunks(embeds, 10))
+    for embed_list in embeded_partition:
+        title = '2020年7月6日のヘッドラインニュース ({}/{})'.format(iterate,
+                                                      len(embeded_partition))
+        webhook.post_link(title, embeds)
+        time.sleep(1)
+        iterate += 1
