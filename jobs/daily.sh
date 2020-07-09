5813e294784413027a28b8b351820c0e2e91efde
Seiichi Yonezawa <nzwsch@gmail.com>
2020-07-09 14:46:42 +0900

    Update crontab

--- jobs/daily.sh
+++ jobs/daily.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+datearg=$(date "+%Y%m%d")
+
 curl -X POST -H "Content-Type: application/json" \
-    -d '{"args":[2,3]}' \
-    http://flower:5555/api/task/async-apply/dailytasks.tasks.mul
+    -d '{"args":["${datearg}"]}' \
+    http://flower:5555/api/task/async-apply/dailytasks.tasks.add
