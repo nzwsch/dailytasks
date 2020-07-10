8b8aa58731ef3cc21467dfd2826b77bd792c1f21
Seiichi Yonezawa <nzwsch@gmail.com>
2020-07-10 23:04:46 +0900

    Fix test_tasks.py

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -10,6 +10,6 @@ def test_get_result_json_returns_expected_path():
     assert get_result_json('20200710') == expected_path
 
 
-def test_get_result_json_returns_writable_dir():
-    writable_dir = Path(__file__).parent.parent.joinpath('json')
-    assert os.access(writable_dir, os.W_OK)
+def test_get_result_json_returns_readable_and_writable_dir():
+    expected_dir = Path(__file__).parent.parent.joinpath('json')
+    assert os.access(expected_dir, os.R_OK | os.W_OK)
